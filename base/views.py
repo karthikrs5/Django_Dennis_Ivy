7f16d03bc647f9a4222cfc52f0f768bef6f3b2e1
karthikrs5 <karthikrs852k@gmail.com>
2023-05-25 12:13:44 +0530

    Moved the responses from urls.py to views.py

--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+#importing HttpResponse to send response to browser
+from django.http import HttpResponse
+
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Home")
+
+def room(request):
+    return HttpResponse("ROOM")
--- studybud/urls.py
+++ studybud/urls.py
@@ -1,22 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
-#This is required to send response to the browser
-from django.http import HttpResponse
-
-#simple function to send response
-def home(request):
-    return HttpResponse('Home Page')
-
-#Adding another route
-def room(request):
-    return HttpResponse('ROOM')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path 
-    path('',home),
 
-    #adding path to the new route "ROOM"
-    path('room/',room),
+    #inlcuding the urls form the "base" app
+    path('',include('base.urls')),
 ]
