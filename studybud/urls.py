1079aa9d331a5328a18e273a46c576daf6c234ca
karthikrs5 <karthikrs852k@gmail.com>
2023-05-25 11:12:48 +0530

    Added a simple response inside urls.py and a path

--- studybud/urls.py
+++ studybud/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for studybud project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+#This is required to send response to the browser
+from django.http import HttpResponse
+
+#simple function to send response
+def home(request):
+    return HttpResponse('Home Page')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path 
+    path('',home),
 ]
