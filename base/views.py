9d4ef65a4b8939f7adcc93d074ee58ab6707762b
karthikrs5 <karthikrs852k@gmail.com>
2023-05-26 11:59:20 +0530

    "for" template tag in django and passing a dictionary value to render()

--- base/views.py
+++ base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 # Create your views here.
 
+rooms=[
+    { 'id' :1 , 'name' :'Lets Learn Python!' },
+    { 'id' :2 , 'name' :'Lets Learn JavaScript!' },
+    { 'id' :3 , 'name' :'Lets Learn Django!' },
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html',{'rooms':rooms})
 
 def room(request):
     return render(request, 'room.html')
