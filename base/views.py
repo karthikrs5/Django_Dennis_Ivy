8ef1b33934456c41c165834ce4214c3007d968be
karthikrs5 <karthikrs852k@gmail.com>
2023-06-05 23:08:28 +0530

    Added User login and logout functionality

--- base/views.py
+++ base/views.py
@@ -6,7 +6,41 @@ from .models import Room,Topic
 
 from .forms import RoomForm
 
+from django.contrib.auth.models import User
+
+from django.contrib.auth import authenticate, login,logout
+
+#imports flash messages
+from django.contrib import messages
+
 # Create your views here.
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+        
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+        
+    context={}
+    return render(request, 'base/login_register.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 
 
 def home(request):
